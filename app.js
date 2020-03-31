e0f3d910c46a4055d45cb5b89cb34a258c339c0d
Rodrigo Sanchez <sanchezgaos@protonmail.com>
2020-03-30 21:51:32 -0600

    Add docker files

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = 3001;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Proxy Server listening on port: ${port}...`);
@@ -13,7 +15,7 @@ app.listen(port, () => {
 
 app.get('/streamers/random', (req, res) => {
   console.log('Fetching data on Random Streamer...');
-  axios.get('http://localhost:8001/streamers/random')
+  axios.get('http://54.176.192.211/streamers/random')
     .then((responsefromService) => {
       console.log(Object.keys(responsefromService.data));
       res.send(responsefromService.data);
@@ -21,7 +23,7 @@ app.get('/streamers/random', (req, res) => {
 });
 
 app.get('/api/get', (req, res) => {
-  axios.get('http://localhost:3000/api/get')
+  axios.get('http://18.144.87.148/api/get')
     .then((responsefromService) => {
       console.log(Object.keys(responsefromService.data));
       res.send(responsefromService.data);
@@ -32,7 +34,7 @@ app.put('/streamers/:name', (req, res) => {
   const streamerName = req.params.name;
   const updateObject = req.body;
   
-  axios.put(`http://localhost:8001/streamers/${streamerName}`, updateObject)
+  axios.put(`http://54.176.192.211/streamers/${streamerName}`, updateObject)
     .then((responsefromService) => {
       res.send(responsefromService.data);
     })
